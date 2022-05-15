b882c693147d6a8b6e56d887abe33411dd341877
MichaelOdermatt <michaelodermatt@outlook.com>
2022-05-15 14:23:41 -0600

    made example_source an xml file

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿namespace XMLDiff_Readable_Diffgram 
+﻿
+namespace XMLDiff_Readable_Diffgram 
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string sourcePath = String.Format(@"{0}/example_source.xml", Directory.GetCurrentDirectory());
+
+            using(StreamReader reader = File.OpenText(sourcePath)) {
+                
+            }
         }
     }
 }
