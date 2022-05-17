b77acfb049c07784afd42e8f44a330e78371e5b3
MichaelOdermatt <michaelodermatt@outlook.com>
2022-05-16 20:38:05 -0600

    renamed file, read source as XmlDoc

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using System.Xml;
+using System.Xml.Linq;
+
 namespace XMLDiff_Readable_Diffgram 
 {
     class Program
@@ -6,9 +8,19 @@ namespace XMLDiff_Readable_Diffgram
         static void Main(string[] args)
         {
             string sourcePath = String.Format(@"{0}/example_source.xml", Directory.GetCurrentDirectory());
+            string diffgramPath = String.Format(@"{0}/example_diffgram.xml", Directory.GetCurrentDirectory());
 
-            using(StreamReader reader = File.OpenText(sourcePath)) {
-                
+            XElement source = XElement.Load(File.OpenText(sourcePath));
+            XElement diffgram = XElement.Load(File.OpenText(diffgramPath));
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(sourcePath);
+            XmlElement? root = doc.DocumentElement; 
+            XmlNodeList? nodes = root.ChildNodes;
+
+            foreach (XmlNode node in nodes)
+            {
+                Console.WriteLine(node.OuterXml);
             }
         }
     }
